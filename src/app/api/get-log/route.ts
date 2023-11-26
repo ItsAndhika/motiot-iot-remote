f6ff73163c0db5769e8c960fd5bdd0eada6c533f
ItsAndhika <bambangindaryanto12@gmail.com>
2023-11-26 17:18:20 +0700

    Change the data fetching sort from the latest document

--- src/app/api/get-log/route.ts
+++ src/app/api/get-log/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { Log } from "@/models/Log";
 
 export async function GET() {
-	const log = await Log.find();
+	const log = await Log.find().sort({ _id: -1 });
 	return NextResponse.json(log);
 }
