72b1f81d7841346b0a7c1bc05a13db8c5c5982c7
ItsAndhika <bambangindaryanto12@gmail.com>
2023-11-26 20:39:13 +0700

    Update the test db

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { Log } from "@/models/Log";
 
 export async function GET() {
-	const newDocument = new Log({ status: "on", lightValue: 500 });
-	await newDocument.save();
+	// const newDocument = new Log({ status: "on", lightValue: 500 });
+	// await newDocument.save();
 	const result = await Log.find({});
 	return NextResponse.json(result);
 }
