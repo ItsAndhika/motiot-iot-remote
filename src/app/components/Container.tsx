a908fd50bae305cd78b86d099523beea6d01769d
ItsAndhika <bambangindaryanto12@gmail.com>
2024-01-13 19:48:58 +0700

    Fix failed when fetching the data that blocked by cors

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,44 +1,27 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import ToggleAutomatic from "./ToggleAutomatic";
 
 export default function Container() {
 	const [isAutomatic, setIsAutomatic] = useState<boolean>(true);
 
 	useEffect(() => {
-		fetch("https://motiot.vercel.app/api/get-automatic-mode", {
+		fetch("/api/get-automatic-mode", {
 			next: { revalidate: 1000 },
 		})
 			.then((data) => data.json())
 			.then((data) => setIsAutomatic(data[0].isAutomatic));
 	}, []);
 
-	const handleToggle = async () => {
-		setIsAutomatic((prev: boolean) => !prev);
-		const response = await fetch(
-			"https://motiot.vercel.app/api/set-automatic-mode",
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ isAutomatic }),
-			}
-		);
-		const result = await response.json();
-		console.log(result);
-	};
-
 	return (
 		<main className="w-11/12 h-[90vh] mx-auto flex flex-col justify-center items-center gap-10 bg-white/20 rounded-3xl backdrop-blur-lg shadow-2xl">
 			<h1 className="text-center text-2xl font-bold bg-gradient-to-br from-[#434343] to-black text-transparent bg-clip-text">
 				Lamp Control
 			</h1>
-			<button
-				onClick={handleToggle}
-				className="px-3 py-2 rounded-full bg-gradient-to-br from-[#434343] to-black text-white font-semibold text-xl transition-all duration-1000 hover:scale-105"
-			>
-				{isAutomatic ? "Switch to manual" : "Switch to automatic"}
-			</button>
+			<ToggleAutomatic
+				isAutomatic={isAutomatic}
+				setIsAutomatic={setIsAutomatic}
+			/>
 			<div className={`${!isAutomatic ? "visible" : "invisible"} flex`}>
 				<span>On</span>
 				<div></div>
