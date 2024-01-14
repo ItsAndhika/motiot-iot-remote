3b6568212ccf1d65d664ebc1e17cf1e73824df2a
ItsAndhika <bambangindaryanto12@gmail.com>
2024-01-14 09:41:15 +0700

    Make the ToggleAutomatic component to be a server component

--- src/app/components/ToggleAutomatic.tsx
+++ src/app/components/ToggleAutomatic.tsx
@@ -1,21 +1,30 @@
-"use client";
-import { useState, useEffect } from "react";
+// "use client";
+// import { useState, useEffect } from "react";
 import ToggleAutomaticButton from "./ToggleAutomaticButton";
 
 // interface ToggleAutomaticProps {
 // 	response: Response;
 // }
 
-export default function ToggleAutomatic() {
-	const [isAutomatic, setIsAutomatic] = useState();
+export default async function ToggleAutomatic() {
+	// const [isAutomatic, setIsAutomatic] = useState();
 
-	useEffect(() => {
-		fetch("/api/get-automatic-mode")
-			.then((data) => data.json())
-			.then((data) => setIsAutomatic(data[0].isAutomatic));
-	}, []);
+	// useEffect(() => {
+	// fetch("/api/get-automatic-mode")
+	// 	.then((data) => data.json())
+	// 	.then((data) => setIsAutomatic(data[0].isAutomatic));
+	// }, []);
 	// const result = await response.json();
 	// const isAutomatic = result[0].isAutomatic;
+
+	const response = await fetch(
+		"https://motiot.vercel.app/api/get-automatic-mode",
+		{
+			cache: "no-store",
+		}
+	);
+	const result = await response.json();
+	const isAutomatic = result[0].isAutomatic;
 
 	return (
 		<>
